61d9595b07ab033482f93fb4a6fc0363bdf2ee63
Ruben Valseca <rubn.vp@gmail.com>
2021-12-29 20:16:47 +0100

    tailwind version

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+      extend: {
+        borderWidth: {
+          '5': '5px',
+        },
+      },
+    },
+    plugins: [],
+  }
